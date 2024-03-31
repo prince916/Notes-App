e079f52695829330ac0d9c9a02643d47d5ba2b3e
Your Name <kumarprince08081999@gmail.com>
2024-03-31 23:09:26 +0530

    newtask2

--- src/modals/EditTask.js
+++ src/modals/EditTask.js
@@ -1,9 +1,6 @@
 import React, { useState , useEffect} from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-
-
-
 const EditTaskPopup = ({modal, toggle, updateTask, taskObj}) => {
     const [taskName, setTaskName] = useState('');
     const [description, setDescription] = useState('');
